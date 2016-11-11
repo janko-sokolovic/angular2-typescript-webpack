f68e71840bb90a3553b88fff19a7e77d5d1736c3
jasofalcon <jasofalcon@gmail.com>
2016-11-11 19:27:53 +0100

    add more beers

--- src/app/beer/mock-beers.ts
+++ src/app/beer/mock-beers.ts
@@ -7,5 +7,10 @@ export const BEERS: Beer[] = [
     new Beer("Budweiser", 4.3, "beers/budweiser.jpg"),
     new Beer("Schneider Tap 7", 5.4, "beers/schneider.jpg"),
     new Beer("Erdinger", 5.6, "beers/erdinger.jpg"),
-    new Beer("Hoegaarden", 4.9, "beers/hoegaarden.jpg")
+    new Beer("Hoegaarden", 4.9, "beers/hoegaarden.jpg"),
+    new Beer("Duvel", 8.5, "beers/duvel.jpg"),
+    new Beer("Weihenstephaner", 5.4, "beers/weihenstephaner.jpg"),
+    new Beer("Kozel", 4.8, "beers/kozel.jpg"),
+    new Beer("Blue Moon", 4.0, "beers/blue-moon.jpg"),
+    new Beer("Memminger", 5.3, "beers/memminger.jpg")
 ];
